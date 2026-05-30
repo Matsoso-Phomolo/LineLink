8e32dc20ce3d926ae52771bc741cc6054638e37a
Matsoso-Phomolo <phomolomatsoso@gmail.com>
2026-05-30 12:38:02 +0200

    Create district_risk_logic.py

---
+++ backend/app/district_risk_logic.py
@@ -0,0 +1,9 @@
+calculate_district_collection_health()
+
+calculate_district_risk_distribution()
+
+calculate_high_risk_landlords()
+
+calculate_district_overdue_exposure()
+
+generate_district_risk_summary()
