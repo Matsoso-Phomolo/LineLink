3842666a6fe66b3127bd7f8fd474c75f4ab70473
Matsoso-Phomolo <phomolomatsoso@gmail.com>
2026-05-30 12:53:01 +0200

    Create national_risk_logic.py

---
+++ backend/app/national_risk_logic.py
@@ -0,0 +1,9 @@
+calculate_national_collection_health()
+
+calculate_national_risk_distribution()
+
+calculate_high_risk_districts()
+
+calculate_national_overdue_exposure()
+
+generate_national_risk_summary()
